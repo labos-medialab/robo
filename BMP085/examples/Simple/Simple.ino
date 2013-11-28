0d995e1cab03d6ac01b1170fb6e3ea7617405437
MakinaMk2 <petarcoklo@gmail.com>
2013-11-28 17:20:21 +0100

    + ‘Adxl345/’ + ‘Adxl345/ADXL345.cpp’ + ‘Adxl345/ADXL345.h’ + ‘Adxl345/License.txt’ + ‘BMP085/BMP085.cpp’ + ‘BMP085/BMP085.h’ + ‘BMP085/examples/Simple/Simple.ino’

---
+++ BMP085/examples/Simple/Simple.ino
@@ -0,0 +1,56 @@
+//Arduino 1.0+ Only
+//Arduino 1.0+ Only
+
+/*Based largely on code by  Jim Lindblom
+
+ Get pressure, altitude, and temperature from the BMP085.
+ Serial.print it out at 9600 baud to serial monitor.
+ */
+#include <Wire.h>
+#include <BMP085.h>
+
+void setup(){
+  Serial.begin(9600);
+  
+
+  bmp085Calibration();
+}
+
+void loop()
+{
+  float temperature = bmp085GetTemperature(bmp085ReadUT()); //MUST be called first
+  float pressure = bmp085GetPressure(bmp085ReadUP());
+  float atm = pressure / 101325; // "standard atmosphere"
+  float altitude = calcAltitude(pressure); //Uncompensated caculation - in Meters 
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature, 2); //display 2 decimal places
+  Serial.println("deg C");
+
+  Serial.print("Pressure: ");
+  Serial.print(pressure, 0); //whole number only.
+  Serial.println(" Pa");
+
+  Serial.print("Standard Atmosphere: ");
+  Serial.println(atm, 4); //display 4 decimal places
+
+  Serial.print("Altitude: ");
+  Serial.print(altitude, 2); //display 2 decimal places
+  Serial.println(" M");
+
+  Serial.println();//line break
+
+  delay(1000); //wait a second and get values again.
+}
+
+
+
+
+
+
+
+
+
+
+
+
