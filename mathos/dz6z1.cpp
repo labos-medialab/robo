0e81f8af58ebd4ff0333b5eb7dab8847a15db865
MakinaMk2 <petarcoklo@gmail.com>
2014-05-06 17:29:38 +0200

    / ‘mathos/dz6z1.cpp’

--- mathos/dz6z1.cpp
+++ mathos/dz6z1.cpp
@@ -10,10 +10,7 @@ protected:
 public:
 	Tocka(): x(0), y(0){};
 	Tocka(float x, float y): x(x), y(y){};
-	Tocka(const Tocka& T){
-		this->x=T.x;
-		this->y=T.y;
-	};
+	Tocka(const Tocka& T): x(T.x), y(T.y){};
 
 	Tocka& operator=(const Tocka& T){
 		this->x=T.x;
@@ -22,13 +19,11 @@ public:
 	};
 
 	friend float d(const Tocka &T1, const Tocka &T2);
-	friend float k(const Tocka &T1, const Tocka &T2);
-	friend float s(const Tocka &T1, const Tocka &T2, const Tocka &T3);
-	friend float r(const Tocka &B, const Tocka &A, const Tocka &C);
+	friend float phi(const Tocka &T1, const Tocka &T2, const Tocka &T3);
 	friend ostream& operator<<(ostream& buffer, const Tocka& z);
 	friend class Trokut;
 
-	Tocka * makeMeMnogokut(const Tocka &S, const float &N, const float &R);
+//	Tocka * makeMeMnogokut(const Tocka &S, const float &N, const float &R);
 };
 
 class Lik{
@@ -44,7 +39,7 @@ protected:
 	Tocka A,B,C;
 public:
 	Trokut();
-	Trokut::Trokut(Tocka &A, Tocka &B, Tocka &C): A(A),B(B),C(C){
+	Trokut(Tocka &A, Tocka &B, Tocka &C): A(A),B(B),C(C){
 		if(isTrokut()){
 			cout << "Trokut: " << endl;
 			cout << "Vrhovi: "<< A << B << C << endl;
@@ -55,16 +50,7 @@ public:
 
 	float povrsina(){return sqrt(opseg()/2*(opseg()/2-d(A,B))*(opseg()/2-d(B,C))*(opseg()/2-d(C,A)));};
 	float opseg(){return d(A,B)+d(B,C)+d(C,A);};
-	float radiusUpKruz(){
-		float r1=r(A,B,C);
-		float r2=r(C,A,B);
-		float r3=r(B,C,A);
-
-		if(abs(r1-r2)<0.00001) return r1;
-		if(abs(r2-r3)<0.00001) return r2;
-		if(abs(r3-r1)<0.00001) return r3;
-		return 0;
-	};
+	float radiusUpKruz(){return 2*povrsina()/opseg();};
 
 	bool isTrokut();
 };
@@ -107,6 +93,7 @@ public:
 		}
 	};
 
+	~PravilniPoligon(){delete [] vrhovi;};
 	float povrsina(){
 		float Alfa=PI-(((N-2)*PI)/N);
 		float R=d(vrhovi[0],vrhovi[1])/(2*sin(Alfa/2));
@@ -130,7 +117,17 @@ public:
 		}
 
 		float FI=((N-2)*PI)/N;
-		//cout << FI << endl;
+		float *fi;
+		fi = new float [N];
+//		cout << FI << endl;
+		for(int i=1; i<N-1; i++){
+			fi[i]=phi(vrhovi[i-1], vrhovi[i], vrhovi[i+1]);
+		}
+		fi[0]=phi(vrhovi[N-1],vrhovi[0],vrhovi[1]);
+		fi[N-1]=phi(vrhovi[N-2],vrhovi[N-1],vrhovi[0]);
+		for(int i=0; i<N; i++){
+			if( fi[i] != FI) return 0;
+		}
 		return 1;
 	};
 };
@@ -147,6 +144,15 @@ float d(const Tocka &T1, const Tocka &T2){
 	return sqrt((T2.x-T1.x)*(T2.x-T1.x) + (T2.y-T1.y)*(T2.y-T1.y));
 }
 
+float phi(const Tocka &T1, const Tocka &T2, const Tocka &T3){
+	float a_x=T2.x-T1.x, a_y=T2.y-T1.y;
+	float b_x=T3.x-T2.x, b_y=T3.y-T2.y;
+	float ab=a_x*b_x+a_y*b_y;
+	float n_a=sqrt(a_x*a_x+a_y*a_y);
+	float n_b=sqrt(b_x*b_x+b_y*b_y);
+	return acos(ab/(n_a*n_b));
+}
+
 ostream& operator<<(ostream& buffer, const Tocka& T){
 	return buffer << "T(" << T.x << ", " << T.y << ") ";
 };
@@ -154,34 +160,6 @@ ostream& operator<<(ostream& buffer, const Tocka& T){
 float povrsinaTrokuta(const Tocka &T1, const Tocka &T2, const Tocka &T3){
 	float s = (d(T1,T2)+d(T2,T3)+d(T3,T1))/2;
 	return sqrt(s*(s-d(T1,T2))*(s-d(T2,T3))*(s-d(T3,T1)));
-}
-
-float r(const Tocka &B, const Tocka &A, const Tocka &C){
-	float a,b,c;
-	a = d(B,C);
-	b = d(A,C);
-	c = d(A,B);
-
-	float s = (a+b+c)/2;
-
-	float k1=k(A,B), k2=k(A,C), fi;
-	if(k2 == -k1) fi = PI/2;
-	else {
-		fi = abs(atan((k2-k1)/(1+k1*k2)));
-	}
-
-	if((k1<0 && k2>0) || (k2<0 && k1>0)){
-		if(fi > 1.57079633 && (a<b || a<c))fi=PI-fi;
-	}
-	
-//	cout << fi*180/PI << endl;
-//	cout << (s-a)*tan(fi/2) << endl;
-	return (s-a)*tan(fi/2);
-}
-
-float k(const Tocka &T1, const Tocka &T2){
-	if(T1.x == T2.x) return 0;
-	return (T2.y-T1.y)/(T2.x-T1.x);
 }
 
 bool Trokut::isTrokut(){
