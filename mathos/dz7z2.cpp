27ceedc6a8f92786093830d1a0c64bd3e21eb888
MakinaMk2 <petarcoklo@gmail.com>
2014-05-12 23:44:11 +0200

    / ‘mathos/dz7z2.cpp’

--- mathos/dz7z2.cpp
+++ mathos/dz7z2.cpp
@@ -1,87 +1,87 @@
 ﻿#include<iostream>
 using namespace std;
 
-template<class T>
-void sort(T *x, int N){
+template<class T, int n>
+class Tuple
+{
+public:
+	T *podaci;
+	Tuple();
+	Tuple(T *input){
+		podaci = new T[n];
+		for(int i=0; i<n; i++) podaci[i]=input[i];
+	};
+	~Tuple(){delete [] podaci;};
+	
+	bool operator<(const Tuple &T) const;
+	bool operator>(const Tuple &T) const;
+
+	void ispis(){
+		cout << "(";
+		for(int i=0; i<n-1; i++){
+			cout << Ti.podaci[i] << ", ";
+		}
+		cout << Ti.podaci[n-1] << ")" << endl;
+	};
+};
+
+template<class T, int n>
+void sort(Tuple<T,n> &Ti){
 	bool done=1;
 	do{
+		T tmp;
 		done = 1;
-		for(int i=0; i<N-1; i++){
-			if(x[i]>x[i+1]){
+		for(int i=0; i<n-1; i++){
+			if(Ti.podaci[i]>Ti.podaci[i+1]){
 				done=0;
-				T tmp = x[i+1];
-				x[i+1]=x[i];
-				x[i]=tmp;
+				tmp = Ti.podaci[i+1];
+				Ti.podaci[i+1]=Ti.podaci[i];
+				Ti.podaci[i]=tmp;
 			}
 		}
 	}
 	while(!done);
 };
 
-class Par{
-	float x, y;
-public:
-	Par(): x(0), y(0) {};
-	Par(float x, float y): x(x), y(y) {};
-	Par(const Par &T): x(T.x), y(T.y) {};
-
-	bool operator<(const Par &P)const{return(x<T.x);};
-	bool operator>(const Par &P)const{return(x>T.x);};
-
-	friend ostream& operator<<(ostream& buffer, const Par& T)
-	{return buffer << "(" << T.x << "," << T.y << ")";};
+template<class T, int n>
+void ispis(Tuple<T,n> &Ti){
+	cout << "(";
+	for(int i=0; i<n-1; i++){
+		cout << Ti.podaci[i] << ", ";
+	}
+	cout << Ti.podaci[n-1] << ")" << endl;
 };
 
 int main(){
-	Par P1(10,0), P2(1,0);
+	int *T1;
+	const int n1=3;
+	T1 = new int [n1];
 
-	int *x, N=5;
-	x = new int[N];
-	x[0]=5;
-	x[1]=4;
-	x[2]=3;
-	x[3]=2;
-	x[4]=1;
+	T1[0] = 17;
+	T1[1] = 21;
+	T1[2] = 11;
 
-	sort(x,N);
+	Tuple <int, n1> To1(T1);
+	
+	cout << "T1";ispis(To1);
+	sort(To1);
+	cout << "T1";ispis(To1);
+	cout << endl;
 
-	cout << x[0];
-	for(int i=1; i<N; i++){
-		cout << ", " << x[i];
-	}
-	cout << ";" << endl;
+	float *T2;
+	const int n2=4;
+	T2 = new float[n2];
 
-	float *y; N=5;
-	y = new float[N];
-	y[0]=(float)5.1;
-	y[1]=(float)4.2;
-	y[2]=(float)3.3;
-	y[3]=(float)2.4;
-	y[4]=(float)1.5;
+	T2[0] = (float)11.52;
+	T2[1] = (float)8.63;
+	T2[2] = (float)1.34;
+	T2[3] = (float)6.24;
 
-	sort(y,N);
+	Tuple <float, n2> To2(T2);
+	
+	cout << "T2";ispis(To2);
+	sort(To2);
+	cout << "T2";ispis(To2);
 
-	cout << y[0];
-	for(int i=1; i<N; i++){
-		cout << ", " << y[i];
-	}
-	cout << ";" << endl;
-
-	Par *z; N=5;
-	z = new Par[N];
-	z[0]=Par(5,75);
-	z[1]=Par(4,75);
-	z[2]=Par(3,75);
-	z[3]=Par(2,75);
-	z[4]=Par(1,75);
-
-	sort(z,N);
-
-	cout << z[0];
-	for(int i=1; i<N; i++){
-		cout << ", " << z[i];
-	}
-	cout << ";" << endl;
-
-	return 0;
+	return 1;
 }
